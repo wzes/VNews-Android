b97a451070349fd0d80c89cf8fac43badc43cdf9
wzes <793890838@qq.com>
2017-11-30 15:47:23 +0800

    add intro activities

---
+++ app/src/main/java/com/mobile/vnews/activity/launch/LaunchActivity.java
@@ -0,0 +1,24 @@
+package com.mobile.vnews.activity.launch;
+
+import android.os.Bundle;
+
+import com.mobile.vnews.R;
+import com.mobile.vnews.activity.BaseActivity;
+
+public class LaunchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_launch;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+}
